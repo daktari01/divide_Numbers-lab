e0ec1143412ef272f48dd4504bde909c1f99a096
Daktari <dindijjames@gmail.com>
2017-10-10 03:00:43 +0000

    Edited the testDivideNumbers

--- divideNumbers.py
+++ divideNumbers.py
@@ -1,6 +1,5 @@
 def divideNumbers(numerator,denominator):
     if denominator == 0:
-        print("Division by zero not allowed!")
-    else:
-        return numerator/denominator
+        raise ValueError("Cannot divide by zero!")
+    return numerator/denominator
     
--- testDivideNumbers.py
+++ testDivideNumbers.py
@@ -1,6 +1,25 @@
-""" Test our divide number function """
+""" Test our divideNumbers function """
 
 import unittest
+
 from divideNumbers import divideNumbers
 
-
+class TestDivideNumbers(unittest.TestCase):
+    """Test divideNumbers functionality"""
+    def test_division(self):
+        self.assertEqual(divideNumbers(10, 5), 2)
+        self.assertEqual(divideNumbers(-1, 1), -1)
+        self.assertEqual(divideNumbers(5, 2), 2.5)
+        
+    def test_raises_exception(self):
+        with self.assertRaises(ValueError):
+            divideNumbers(4, 0)
+        
+    def test_input_is_not_a_list(self):
+        with self.assertRaises(TypeError):
+            divideNumbers([2, 3], [1,2])
+            divideNumbers([2, 3], 3)
+            divideNumbers(4, [1,2])
+        
+if __name__ == '__main__':
+    unittest.main()
